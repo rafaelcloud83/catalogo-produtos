8a6b0991b3cbdc43b3a9126fa74de82d251ba657
rafaelcloud83 <rafaelcloud83@gmail.com>
2024-02-11 20:44:10 -0300

    feat:created ProductDTO

---
+++ src/main/java/edu/rafael/catalogoprodutos/dto/ProductDto.java
@@ -0,0 +1,102 @@
+package edu.rafael.catalogoprodutos.dto;
+
+import edu.rafael.catalogoprodutos.entities.Category;
+import edu.rafael.catalogoprodutos.entities.Product;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    private Instant date;
+
+    private List<CategoryDto> categories = new ArrayList<>();
+
+    public ProductDto() {
+    }
+
+    public ProductDto(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public ProductDto(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+        this.imgUrl = product.getImgUrl();
+        this.date = product.getDate();
+    }
+
+    public ProductDto(Product product, Set<Category> categories) {
+        this(product);
+        categories.forEach(cat -> this.categories.add(new CategoryDto(cat)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public List<CategoryDto> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryDto> categories) {
+        this.categories = categories;
+    }
+}
